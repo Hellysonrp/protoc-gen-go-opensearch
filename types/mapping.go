5ff90e95cc1ecad0724398907a5667cf44badcad
Hellyson Rodrigo Parteka <hellysonrp@gmail.com>
2022-09-30 10:18:19 -0300

    feat: add dynamic templates

--- types/mapping.go
+++ types/mapping.go
@@ -1,10 +1,18 @@
 package types
 
+type OpensearchDynamicTemplate struct {
+	MatchMappingType string            `json:"match_mapping_type,omitempty"`
+	Match            string            `json:"match,omitempty"`
+	Unmatch          string            `json:"unmatch,omitempty"`
+	Mapping          OpensearchMapping `json:"mapping"`
+}
+
 type OpensearchMapping struct {
-	Analyzer    string                       `json:"analyzer,omitempty"`
-	Type        string                       `json:"type,omitempty"`
-	Properties  map[string]OpensearchMapping `json:"properties,omitempty"`
-	Fields      map[string]OpensearchMapping `json:"fields,omitempty"`
-	Index       *bool                        `json:"index,omitempty"`
-	IgnoreAbove int                          `json:"ignore_above,omitempty"` // used with keyword field type
+	Analyzer         string                                 `json:"analyzer,omitempty"`
+	Type             string                                 `json:"type,omitempty"`
+	Properties       map[string]OpensearchMapping           `json:"properties,omitempty"`
+	Fields           map[string]OpensearchMapping           `json:"fields,omitempty"`
+	Index            *bool                                  `json:"index,omitempty"`
+	IgnoreAbove      int                                    `json:"ignore_above,omitempty"` // used with keyword field type
+	DynamicTemplates []map[string]OpensearchDynamicTemplate `json:"dynamic_templates,omitempty"`
 }
